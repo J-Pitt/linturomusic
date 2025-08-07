13a73f93734460c35cbbce34a2d25fec420be9f2
Jason Pittman <jpittman@Jasons-MacBook-Pro.local>
2025-08-06 21:23:05 -0400

    moving music wave closer to buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -157,7 +157,7 @@ const Hero = () => {
 
       {/* Audio waves animation - positioned dynamically based on music state */}
       <div className={`absolute left-1/2 transform -translate-x-1/2 flex space-x-3 transition-all duration-500 ease-in-out ${
-        isPlaying ? 'bottom-8 sm:bottom-12' : 'bottom-48 sm:bottom-52'
+        isPlaying ? 'bottom-20 sm:bottom-24' : 'bottom-48 sm:bottom-52'
       }`}>
         {[...Array(8)].map((_, i) => (
           <motion.div
@@ -380,7 +380,7 @@ const Hero = () => {
         animate={{ opacity: 1 }}
         transition={{ duration: 1, delay: 1.2 }}
         className={`flex justify-center absolute left-1/2 transform -translate-x-1/2 ${
-          isPlaying ? 'bottom-0 sm:bottom-2' : 'bottom-32 sm:bottom-36'
+          isPlaying ? 'bottom-8 sm:bottom-12' : 'bottom-32 sm:bottom-36'
         } transition-all duration-500 ease-in-out`}
       >
         <motion.button
